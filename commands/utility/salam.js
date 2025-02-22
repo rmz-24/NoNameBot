ad1dde54c49b83d303c028490181410c1fa673b3
Ramzi <mramzit.pro@gmail.com>
2025-02-22 23:11:13 +0100

    Modified the salam command. Added time wait.

--- commands/utility/salam.js
+++ commands/utility/salam.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, time } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("salam")
         .setDescription("Replies with salam"),
     async execute(interaction) {
-        await interaction.reply("salam");
+        if(interaction.user.id == '562335433057370153'){
+            await interaction.reply("Salam wassim!");
+            setTimeout(() => {
+                interaction.editReply("Khlass 9wd douka!");
+            }, 3000);
+        } else {
+            await interaction.reply("Salam !");
+        }
     },
 };
