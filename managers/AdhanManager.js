b7987512008c00c621f026120df2daac068d074e
waSStyle <34811606+waSStyle@users.noreply.github.com>
2025-02-23 11:51:35 +0100

    Created a method that kicks all other bots from voice channels

--- managers/AdhanManager.js
+++ managers/AdhanManager.js
@@ -2,5 +2,17 @@ const cron = require("node-cron");
 const {joinVoiceChannel,createAudioPlayer,createAudioResource} = require("@discordjs/voice");
 const adhan = require("adhan");
 
+function kickAllBots(client) {
+    client.guilds.cache.forEach(guild => {
+        guild.members.cache
+            .filter(member => member.user.bot && member.user.id !== client.user.id)
+            .forEach(member => {
+                if(member && member.voice.channel) {
+                    member.voice.disconnect();
+                }
+            });
+    })
+}
+
 module.exports = {
 }
